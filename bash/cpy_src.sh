bd60d0e57c3f80e25c6883273d5806ec23b1d373
Lu Yihui <lu_yihui@126.com>
2019-12-26 22:58:45 +0800

    bash for src_cpy

---
+++ bash/cpy_src.sh
@@ -0,0 +1,7 @@
+cp ./fork.c /home/linux/kernel-src/pksm-4.2/kernel/fork.c
+cp ./memory.c /home/linux/kernel-src/pksm-4.2/mm/memory.c
+cp ./pksm.c /home/linux/kernel-src/pksm-4.2/mm/ksm.c
+cp ./page_alloc.c /home/linux/kernel-src/pksm-4.2/mm/page_alloc.c
+cp ./pksm.h /home/linux/kernel-src/pksm-4.2/include/linux/ksm.h
+cp ./config /home/linux/kernel-src/pksm-4.2/.config
+
