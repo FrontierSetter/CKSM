08d00db97cb321dca3b697f69da3e22152854c9c
Lu Yihui <lu_yihui@126.com>
2020-04-24 20:59:10 +0800

    vmware docker usage

--- workload/docker_start/elasticsearch/start_elastic.sh
+++ workload/docker_start/elasticsearch/start_elastic.sh
@@ -7,7 +7,7 @@ do
     echo "$name$i"
 	date +%s >> ${my_log}"timestamp_elastic.log"
 
-    docker run -d -e "discovery.type=single-node" elasticsearch:7.5.1
+    docker run -d -e "discovery.type=single-node" elasticsearch:7.6.2
     sleep 3
     # docker run -d kibana:7.5.1
     # sleep 6
