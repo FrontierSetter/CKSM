d1df1c532e413228291f0eb5d0a9660fafd59b4f
Lu Yihui <lu_yihui@126.com>
2020-05-29 22:55:38 +0800

    draw line py

---
+++ draw_tool/preparse.py
@@ -0,0 +1,31 @@
+import sys
+import os.path
+
+# 输出两列数据，用逗号分隔：时间戳，当时内存使用情况Byte
+# 第一行为数据备注
+# 第二行为基本参数：起始时间戳，内存基准
+# 第一二行的信息需要人工填写
+
+inputFilePath = sys.argv[1]
+pathTuple = os.path.split(inputFilePath)
+print(pathTuple)
+
+inputFile = open(inputFilePath, 'r')
+outputFile = open(os.path.join(pathTuple[0], 'out_'+pathTuple[1]), 'w')
+
+
+while True:
+    curLine = inputFile.readline().strip('\n')
+    if curLine == "":
+        break
+
+    curTime = int(curLine)
+    inputFile.readline()
+    curLineArr = [x for x in inputFile.readline().strip('\n').split(' ') if x]
+    curMem = int(curLineArr[2])
+    inputFile.readline()
+
+    outputFile.write("%d,%d\n" % (curTime, curMem))
+
+outputFile.close()
+inputFile.close()
